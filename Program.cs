117c3254264d90f767c0d67665d0e9e6ca0b6065
Victoria <victoria.babchenko30@gmail.com>
2022-12-05 13:21:14 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Второй_максимум
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            var nums = new List<int>() {};
+            while (num != 0)
+            {
+                nums.Add(num);
+                num = int.Parse(Console.ReadLine());
+            }
+            nums.Sort((x, y) => y.CompareTo(x));
+            Console.WriteLine(nums[1]);
+        }
+    }
+}
